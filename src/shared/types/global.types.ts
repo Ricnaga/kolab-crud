286a2462e5f478d8709570f58d99552c27bdce09
ricnaga <ric.nagato@gmail.com>
2024-12-26 15:43:07 -0300

    feat: Add global types | Add request types

---
+++ src/shared/types/global.types.ts
@@ -0,0 +1,44 @@
+export type PostType = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+export type CommentType = {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+};
+
+type GeoType = {
+  lat: string;
+  lng: string;
+};
+
+type AddressType = {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: GeoType;
+};
+
+type CompanyType = {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+};
+
+export type UserType = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: AddressType;
+  company: CompanyType;
+};
