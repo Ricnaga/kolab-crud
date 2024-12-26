0711ecab66dc78672bec49199855abbd31f26090
ricnaga <ric.nagato@gmail.com>
2024-12-26 18:54:01 -0300

    refactor: Update theme

--- src/application/theme/chakra-ui/Provider.tsx
+++ src/application/theme/chakra-ui/Provider.tsx
@@ -1,11 +1,15 @@
-import { Box, ChakraProvider } from "@chakra-ui/react";
-import { system } from "@chakra-ui/react/preset";
+import { Box, ChakraProvider, createSystem } from "@chakra-ui/react";
+import { defaultConfig } from "@chakra-ui/react/preset";
 import { ReactNode } from "react";
+
+const system = createSystem(defaultConfig, { preflight: true });
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   return (
     <ChakraProvider value={system}>
-      <Box padding={{ base: 2, sm: "2", md: "6" }}>{children}</Box>
+      <Box padding={{ base: 2, sm: "2", md: "6" }} background={"green.50"}>
+        {children}
+      </Box>
     </ChakraProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AppRouter } from "./application/router/AppRouter.tsx";
 import ComposeDataProviders from "./context/Compose.tsx";
-import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
