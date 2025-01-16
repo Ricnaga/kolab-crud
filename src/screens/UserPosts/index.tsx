738deeadc795469030f521b9c98a391c93b5d121
ricnaga <ric.nagato@gmail.com>
2025-01-16 18:14:15 -0300

    refactor: Update user posts page

--- src/screens/UserPosts/index.tsx
+++ src/screens/UserPosts/index.tsx
@@ -1,62 +1,42 @@
-import { usePostsQuery } from "@/infra/posts";
-import { useUserQuery } from "@/infra/users";
-import { Button, Card, Heading, SimpleGrid, Text } from "@chakra-ui/react";
-import { CardSkeleton } from "@/screens/Home/components/CardPost/components/CardSkeleton";
+import {
+  Button,
+  Flex,
+  Grid,
+  GridItem
+} from "@chakra-ui/react";
 import { MdOutlineArrowBack } from "react-icons/md";
-import { useNavigate, useParams } from "react-router";
+import { useNavigate } from "react-router";
+import { CardPosts } from "./components/CardPosts";
+import { HeadingAuthor } from "./components/HeadingAuthor";
 
 export default function UserPosts() {
   const navigate = useNavigate();
-  const { userId } = useParams<{ userId: string }>();
-
-  const { isLoading, data } = usePostsQuery({ userId });
-  const { data: userData } = useUserQuery({
-    id: userId || "",
-  });
-
-  if (isLoading)
-    return (
-      <SimpleGrid gap={2} mt={2}>
-        {Array.from({ length: 3 }).map(() => (
-          <CardSkeleton key={Math.random().toString()} />
-        ))}
-      </SimpleGrid>
-    );
 
   return (
-    <SimpleGrid gap={2}>
-      <Heading>
-        <Button
-          variant={"outline"}
-          borderRadius={"full"}
-          onClick={() => navigate(-1)}
-        >
-          <MdOutlineArrowBack />
-        </Button>{" "}
-        Postagens do usuário: {userData?.name}
-      </Heading>
-      {data.map((post) => (
-        <Card.Root
-          key={post.id.toString()}
-          p={4}
-          display={"grid"}
-          gap={2}
-          shadow={"0px 10px 12px -12px rgba(0,0,0,0.1)"}
-        >
-          <Card.Header>
-            <Text fontWeight={"bold"} textStyle={"xl"}>
-              Título:
-            </Text>{" "}
-            {post.title}
-          </Card.Header>
-          <Card.Body>
-            <Text fontWeight={"bold"} textStyle={"xl"}>
-              Descrição:
-            </Text>{" "}
-            {post.body}
-          </Card.Body>
-        </Card.Root>
-      ))}
-    </SimpleGrid>
+    <Grid gap={4}>
+      <GridItem mt={4}>
+        <Flex alignItems="center" gap={2}>
+          <Button
+            transition="all"
+            _hover={{
+              filter: "brightness(105%)",
+              _active: { filter: "brightness(110%)" },
+            }}
+            backgroundGradient="to-br"
+            gradientFrom="cyan.600"
+            gradientVia="cyan.500"
+            gradientTo="cyan.400"
+            borderRadius={"full"}
+            onClick={() => navigate(-1)}
+          >
+            <MdOutlineArrowBack />
+          </Button>
+          <HeadingAuthor />
+        </Flex>
+      </GridItem>
+      <GridItem display="grid" gap={2} h="80vh" overflowY="auto" p={1}>
+        <CardPosts />
+      </GridItem>
+    </Grid>
   );
 }
