3dc94c87f7198a4761ae9d6864b779bd29d051e9
ricnaga <ric.nagato@gmail.com>
2025-01-16 16:29:36 -0300

    refactor: Change Card title author

--- src/screens/Home/components/CardPost/components/AccordionItemContentComments/components/CardTitleAuthor/CardTitleAuthor.view.tsx
+++ src/screens/Home/components/CardPost/components/AccordionItemContentComments/components/CardTitleAuthor/CardTitleAuthor.view.tsx
@@ -1,28 +1,10 @@
-import { Button, CardTitle, Skeleton } from "@chakra-ui/react";
-import { FaChevronRight } from "react-icons/fa6";
+import { CardTitle, Skeleton } from "@chakra-ui/react";
 import { CardTitleAuthorViewProps } from "./card-title-author.types";
 
 export const CardTitleAuthorView = (props: CardTitleAuthorViewProps) => {
-  const { authorName, isLoading, handleNextPage } = props;
+  const { authorName, isLoading } = props;
 
   if (isLoading) return <Skeleton h={7} />;
 
-  return (
-    <CardTitle display="grid" gap={2} gridTemplateColumns="repeat(2, 1fr)">
-      {authorName}
-      <Button
-        onClick={handleNextPage}
-        justifySelf="flex-end"
-        borderRadius="full"
-        size="xs"
-        bg={{
-          base: "yellow.500",
-          _hover: "yellow.600",
-        }}
-        _active={{ filter: "brightness(105%)" }}
-      >
-        <FaChevronRight />
-      </Button>
-    </CardTitle>
-  );
+  return <CardTitle>{authorName}</CardTitle>;
 };
--- src/screens/Home/components/CardPost/components/AccordionItemContentComments/components/CardTitleAuthor/use-card-title-author.model.ts
+++ src/screens/Home/components/CardPost/components/AccordionItemContentComments/components/CardTitleAuthor/use-card-title-author.model.ts
@@ -1,20 +1,14 @@
 import { useUserQuery } from "@/infra/users";
 import { UseCardTitleAuthorProps } from "./card-title-author.types";
-import { useNavigate } from "react-router";
-import { USER_BYUSER_ID_POSTS } from "@application/router/paths";
 
 export const useCardTitleAuthor = (props: UseCardTitleAuthorProps) => {
-  const navigate = useNavigate();
   const { userId } = props;
 
   const { data, isLoading } = useUserQuery({ id: userId });
 
   const authorName = data?.name || "Desconhecido";
 
-  const handleNextPage = () =>
-    navigate(USER_BYUSER_ID_POSTS.replace(":userId", userId));
-
-  return { authorName, isLoading, handleNextPage };
+  return { authorName, isLoading };
 };
 
 export type UseCardTitleAuthorReturnType = ReturnType<
