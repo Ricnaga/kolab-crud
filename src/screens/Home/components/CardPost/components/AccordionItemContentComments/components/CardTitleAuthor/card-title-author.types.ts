b3461acccecf63ab029122c86a59bc62d3219258
ricnaga <ric.nagato@gmail.com>
2025-01-16 13:43:37 -0300

    feat: Add Card Title author component

---
+++ src/screens/Home/components/CardPost/components/AccordionItemContentComments/components/CardTitleAuthor/card-title-author.types.ts
@@ -0,0 +1,8 @@
+import { UseCardTitleAuthorReturnType } from "./use-card-title-author.model";
+
+export type UseCardTitleAuthorProps = {
+  userId: string;
+};
+
+export type CardTitleAuthorProps = UseCardTitleAuthorProps;
+export type CardTitleAuthorViewProps = UseCardTitleAuthorReturnType;
