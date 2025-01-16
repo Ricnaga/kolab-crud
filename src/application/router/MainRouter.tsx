59a660d9d702fec27be6040b560997733b3beb7a
ricnaga <ric.nagato@gmail.com>
2025-01-16 13:42:00 -0300

    refactor: Update Theme provider

--- src/application/router/MainRouter.tsx
+++ src/application/router/MainRouter.tsx
@@ -2,23 +2,29 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 import { Link, Navigate, Outlet, useLocation } from "react-router";
 import { HOME, MAIN, PROFILE } from "./paths";
 
+const links = {
+  [PROFILE]: "Perfil",
+  [HOME]: "Home",
+};
+
 export function MainRouter() {
   const { pathname } = useLocation();
   if (pathname === MAIN) return <Navigate to={HOME} replace />;
 
   return (
     <main>
-      <Flex gap={2}>
-        <Link to={PROFILE}>
-          <Text _hover={{ color: "yellow.500" }} textStyle={"xl"}>
-            Perfil
-          </Text>
-        </Link>
-        <Link to={HOME}>
-          <Text _hover={{ color: "yellow.500" }} textStyle={"xl"}>
-            Feed de posts
-          </Text>
-        </Link>
+      <Flex gap={2} shadow="md" bg="gray.800" color="white" p={2}>
+        {Object.entries(links).map(([key, value]) => (
+          <Link to={key} key={key}>
+            <Text
+              _hover={{ color: "yellow.200" }}
+              transition="all"
+              textStyle={"xl"}
+            >
+              {value}
+            </Text>
+          </Link>
+        ))}
       </Flex>
       <Box p={2}>
         <Outlet />
--- src/application/theme/chakra-ui/Provider.tsx
+++ src/application/theme/chakra-ui/Provider.tsx
@@ -1,4 +1,4 @@
-import { Box, ChakraProvider, createSystem } from "@chakra-ui/react";
+import { chakra, ChakraProvider, createSystem } from "@chakra-ui/react";
 import { defaultConfig } from "@chakra-ui/react/preset";
 import { ReactNode } from "react";
 
@@ -7,20 +7,24 @@ const system = createSystem(defaultConfig, {
   globalCss: {
     body: {
       height: "100%",
+      backgroundGradient: "to-t",
+      gradientFrom: "yellow.400",
+      gradientVia: "yellow.200",
+      gradientTo: "yellow.100",
     },
+  },
+});
+
+const StyledBox = chakra("div", {
+  base: {
+    h: "100vh",
   },
 });
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   return (
     <ChakraProvider value={system}>
-      <Box
-        padding={{ base: 2, sm: "2", md: "6" }}
-        background={"blue.50"}
-        h={"100vh"}
-      >
-        {children}
-      </Box>
+      <StyledBox>{children}</StyledBox>
     </ChakraProvider>
   );
 }
